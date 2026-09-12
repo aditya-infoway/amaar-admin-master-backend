dbb6304a866d24e698d0a79b43a35c08de520fc8
Nitin <adityagithub311@gmail.com>
2026-09-12 15:29:46 +0530

    itemmastr

--- app/routes/saleexecutive/index.js
+++ app/routes/saleexecutive/index.js
@@ -3,6 +3,8 @@ module.exports = (app) => {
     require("./followup.routes")(app);
     require("./quotation.routes")(app);
     require("./createmaster.routes")(app);
-       require("./model.routes")(app);
+    // require("./model.routes")(app);
+    require("./itemmaster.routes")(app);
+    require("./createpricing.routes")(app);
 
 };
